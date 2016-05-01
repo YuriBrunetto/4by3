3e2b2501ff08f387af9a32010082feb6531aca2e
Yuri Brunetto <yuribrunetto@hotmail.com.br>
2016-05-01 16:24:20 -0300

    add travis

---
+++ test.js
@@ -0,0 +1,15 @@
+var test = require("tape");
+var fbt = require("./index.js");
+
+test("should be 4:3", function(assert) {
+    var result = fbt(800, "");
+    assert.equal(result, "4:3 -> 600")
+    assert.end();
+});
+
+test("should be 16:9", function(assert) {
+    var wide = { wide: true };
+    var result = fbt(1600, wide);
+    assert.equal(result, "16:9 -> 900");
+    assert.end();
+});
